8eeea8f0aa2042e01421e7aeceba31b1205363c1
ownist <shahedevise@gmail.com>
2026-07-31 21:44:57 +0600

    chore(olab): remove legacy web dev assets
    - Remove obsolete web dev batch 03 projects
    - Remove outdated suggestions and PDF resources
    - Clean up unused project assets and dependencies

--- courses/olab/asset_batch_03_web_dev/projects/portfolio-20/js/script.js
+++
@@ -1,8 +0,0 @@
-function displayDateTime() {
-   const dateTime = new Date();
-   const date = dateTime.toLocaleDateString();
-   const time = dateTime.toLocaleTimeString();
-   document.getElementById('dateTime').innerText = date+' '+time;
-}
-setInterval(displayDateTime,1000);
-
